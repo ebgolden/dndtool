ba7ef049ab0f38073670a9f4d327853ac5aecd77
ebgolden <edwardgoldenberg@gmail.com>
2021-05-18 15:44:51 -0700

    Added documentation to JoinParty API to Party Service.

--- src/main/java/services/partyservice/JoinParty.java
+++ src/main/java/services/partyservice/JoinParty.java
@@ -1,5 +1,16 @@
 package services.partyservice;
 
 public interface JoinParty {
+    /**
+     * Returns a JoinPartyResponse containing a joinedParty boolean.
+     * Accepts a Party object, a Character object, a Player object,
+     * and a acceptedByParty boolean in an JoinPartyRequest.
+     * @param joinPartyRequest JoinPartyRequest
+     *                         containing a Party object,
+     *                         a Character object, a
+     *                         Player object, and a
+     *                         acceptedByParty boolean
+     * @return JoinPartyResponse containing a joinedParty boolean
+     */
     JoinPartyResponse getJoinPartyResponse(JoinPartyRequest joinPartyRequest);
 }
